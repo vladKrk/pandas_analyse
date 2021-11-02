8ad8bb59fde29b9ffe91811fa66d7794afa031db
Vladislav Zhavoronkov <vladislav.z@fora-soft.com>
2021-11-02 21:12:58 +0300

    quests 5, 6, 7, 8, 10

--- quest1.py
+++ quest1.py
@@ -28,7 +28,53 @@ print('2. Названия столбцов: ', dataset.columns.tolist())
 print('3. Самая частая позиция в заказе: ', dataset.groupby('item_name')['quantity'].sum().idxmax())
 
 # 4
-hist = dataset.groupby('item_name')['quantity'].sum().hist()
-hist.set_xlabel('quantity')
-hist.set_ylabel('item_name')
-plt.show()
+hist = dataset.groupby('item_name')['quantity'].sum().plot(kind='bar')
+hist.set_ylabel('quantity')
+plt.title('Частота заказов по позициям')
+print('4. Построить гистрограмму частоты заказов по позициям')
+# plt.show()
+
+# 5
+dataset['item_price'] = dataset['item_price'].apply(lambda x: float(x[1:-1]))
+print('5. Измените тип переменной item_price c с помощью лямбды функции: \n', dataset.dtypes)
+
+# 6
+item_price = pd.to_numeric(dataset['item_price'])
+quantity = pd.to_numeric(dataset['quantity'])
+
+dataset['sum_price'] = quantity * item_price
+dataset.groupby('item_name')['sum_price'].sum().plot(kind='bar')
+plt.xlabel('item_name')
+plt.ylabel('Money')
+plt.title('Кол-во денег заработанных по каждой позиции')
+print('6. Построить гистограмму кол-во денег заработанных по каждой позиции')
+# plt.show()
+
+# 7
+print('7. Средняя сумма заказа?')
+print('\t 1 способ: ', dataset['sum_price'].sum() / len(dataset.groupby('order_id')))
+total_order_sum = dataset['sum_price'].sum()
+num_orders = len(set(dataset['order_id']))
+average_order_price = total_order_sum / num_orders
+print('\t 2 способ: ', average_order_price)
+
+# 8
+print("8. Выведите среднее, минимальное и максимальное, медианное значения позиций в заказе")
+print('\t Среднее: ', dataset.quantity.sum() / len(dataset.groupby('order_id')))
+print('\t Минимальное: ', dataset.groupby('order_id').quantity.sum().min())
+print('\t Максимальное: ', dataset.groupby('order_id').quantity.sum().max())
+print('\t Медианное: ', dataset.groupby('order_id').quantity.sum().median())
+
+# 9
+print('9. Определить статистику заказов стейков, а также статистику заказов прожарки.')
+
+# 10
+print('10. Добавить новый столбец цен на каждую позицию в заказе в рублях.')
+dataset['price_rubles'] = dataset['item_price'].apply(lambda value: str(round(value * 71, 2)) + ' RUB')
+print(dataset.head(5))
+
+# 11
+print('11. Сгруппировать заказы по входящим позициям в него. Отдельно сгруппировать по стейкам во всех видах прожарках.')
+
+# 12
+print('12. Определить цену по каждой позиции в отдельности.')
