69f93ec022a4569378a6a1353c93eeef065f908f
Vladislav Zhavoronkov <vladislav.z@fora-soft.com>
2021-11-17 21:34:17 +0300

    Last quests

--- quest1.py
+++ quest1.py
@@ -32,28 +32,24 @@ hist = dataset.groupby('item_name')['quantity'].sum().plot(kind='bar')
 hist.set_ylabel('quantity')
 plt.title('Частота заказов по позициям')
 print('4. Построить гистрограмму частоты заказов по позициям')
-# plt.show()
+plt.show()
 
 # 5
 dataset['item_price'] = dataset['item_price'].apply(lambda x: float(x[1:-1]))
 print('5. Измените тип переменной item_price c с помощью лямбды функции: \n', dataset.dtypes)
 
 # 6
-item_price = pd.to_numeric(dataset['item_price'])
-quantity = pd.to_numeric(dataset['quantity'])
-
-dataset['sum_price'] = quantity * item_price
-dataset.groupby('item_name')['sum_price'].sum().plot(kind='bar')
+dataset.groupby('item_name')['item_price'].sum().plot(kind='bar')
 plt.xlabel('item_name')
 plt.ylabel('Money')
 plt.title('Кол-во денег заработанных по каждой позиции')
 print('6. Построить гистограмму кол-во денег заработанных по каждой позиции')
-# plt.show()
+plt.show()
 
 # 7
 print('7. Средняя сумма заказа?')
-print('\t 1 способ: ', dataset['sum_price'].sum() / len(dataset.groupby('order_id')))
-total_order_sum = dataset['sum_price'].sum()
+print('\t 1 способ: ', dataset['item_price'].sum() / len(dataset.groupby('order_id')))
+total_order_sum = dataset['item_price'].sum()
 num_orders = len(set(dataset['order_id']))
 average_order_price = total_order_sum / num_orders
 print('\t 2 способ: ', average_order_price)
@@ -67,8 +63,7 @@ print('\t Медианное: ', dataset.groupby('order_id').quantity.sum().medi
 
 # 9
 print('9. Определить статистику заказов стейков, а также статистику заказов прожарки.')
-total = dataset[dataset['item_name'].str.contains('Steak')].groupby(['item_name']).agg({"sum_price": "mean", "quantity": "sum", "order_id": "count"}).reset_index()
-total = total.rename(columns={"sum_price": "avg_price_paid", "order_id": "times_ordered"})
+total = dataset[dataset['item_name'].str.contains('Steak')].groupby(['item_name'])['quantity', 'item_price'].describe()
 print(total)
 
 total = dataset['choice_description'].str.split(expand=True).stack().reset_index(level=1, drop=True).to_frame('roasting').merge(dataset, left_index=True, right_index=True)
@@ -78,6 +73,7 @@ total = total.groupby(['roasting']).agg({"quantity": "sum", "order_id": "count"}
 total = total.rename(columns={"order_id": "times_ordered"})
 print(total)
 
+
 # 10
 print('10. Добавить новый столбец цен на каждую позицию в заказе в рублях.')
 dataset['price_rubles'] = dataset['item_price'].apply(lambda value: str(round(value * 71, 2)) + ' RUB')
@@ -85,24 +81,24 @@ print(dataset.head(5))
 
 # 11
 print('11. Сгруппировать заказы по входящим позициям в него. Отдельно сгруппировать по стейкам во всех видах прожарках.')
-group_order = pd.DataFrame(dataset.groupby(['item_name', 'order_id']).agg({"sum_price": "mean", "quantity": "count"}))
-print(group_order)
+group_order = dataset.groupby('order_id').agg({'item_name': lambda x: ", ".join(x), 'item_price': np.sum, 'quantity': np.sum})
+group_order[['quantity', 'item_name']]
 
-total = dataset['choice_description'].str.split(expand=True).stack().reset_index(level=1, drop=True).to_frame('roasting').merge(dataset, left_index=True, right_index=True)
-total = total[total['roasting'].str.contains('Mild|Medium|Hot')]
-total['roasting'] = total.roasting.str.strip(',[]()')
-total = total[total['item_name'].str.contains('Steak')].groupby(['roasting', 'order_id', 'item_name', 'choice_description']).agg({"sum_price": "mean", "quantity": "count"})
-print(total)
+dataset[dataset['item_name'].str.contains('Steak')].groupby('order_id').agg({'quantity':np.sum,
+                                'item_name': lambda x: ", ".join(x), 
+                                'item_price':np.sum})
 
 
 # 12
 print('12. Определить цену по каждой позиции в отдельности.')
-by_item = pd.DataFrame(
-dataset.groupby("item_name").agg({"sum_price": "mean", "quantity": "count"}).reset_index())
-by_item = by_item.rename(columns={"sum_price": "avg_price_paid", "quantity": "times_ordered"})
+contains_and = dataset.item_name.str.contains('and')
+dataset.loc[contains_and, ('price_for_one_item')] = round((dataset['item_price'] - 2.15) / dataset['quantity'], 2)
+if not np.all(contains_and == False):
+    dataset.loc[~contains_and, ('price_for_one_item')] = round(dataset['item_price'] / dataset['quantity'], 2)
+dataset.loc[contains_and, ('item_name')] = dataset[contains_and]['item_name'].apply(lambda row: row[10:])
 
-by_item["revenue"] = by_item["avg_price_paid"] * by_item["times_ordered"]
-
-print(by_item)
+pr_for_1_item = dataset.groupby('item_name').price_for_one_item.agg('unique').reset_index()
+pr_for_1_item["mean_price"] = pr_for_1_item["price_for_one_item"].apply(lambda row: round(np.mean(list(row)), 2))
+pr_for_1_item[~pr_for_1_item.item_name.str.contains('and')]
 
 
